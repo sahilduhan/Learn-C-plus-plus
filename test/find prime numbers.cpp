64d347bd4b9642dcc3a16af9da88faf057ad0759
sahilduhan <sahilduhan16092001@gmail.com>
2020-11-20 20:15:43 +0530

    update string again

--- test/find prime numbers.cpp
+++ test/find prime numbers.cpp
@@ -2,21 +2,24 @@
     given a number n find all prime number till n
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int num;
-vector<int> arr({1,2,3,4,5,6,7,8,9,10});
-int n=arr.size();
-for (int i=0;i<n;i++)
-{
- 
-
-}
-
-
-
-
+    int n = 10;
+    for (int number = 2; number <= n; number++)
+    {
+        bool isPrime = true;
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+            cout << number << endl;
+    }
     return 0;
 }
