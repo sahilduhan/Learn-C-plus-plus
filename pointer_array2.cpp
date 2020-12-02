64f316a7233fb5ebbf0328e0bcd41f3741dcc232
sahilduhan <sahilduhan16092001@gmail.com>
2020-12-02 10:38:42 +0530

    update array pointer

---
+++ pointer_array2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+char c[] ="sahil";
+int len=strlen(c);
+int size=sizeof(c);
+
+cout<<"the length is "<<len<<" and the size in bytes is "<<size<<endl;
+
+return 0;
+
+}
