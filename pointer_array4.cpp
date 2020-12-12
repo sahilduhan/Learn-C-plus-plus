5af97709846c1e5bcad7999bfc5470f225aae845
sahilduhan <sahilduhan16092001@gmail.com>
2020-12-12 23:58:22 +0530

    point_array

--- pointer_array4.cpp
+++ pointer_array4.cpp
@@ -6,6 +6,7 @@ int main()
     double *point_score(scores);
     cout << point_score[2] << endl;
     cout << point_score[0] << endl;
-
+    cout << *(point_score + 1) << endl;
+    cout << scores[0] << endl;
     return 0;
 }
