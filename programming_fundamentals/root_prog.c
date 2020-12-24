bacdfeeb67065b99165744c4a29883b8f46149a2
sahilduhan <sahilduhan16092001@gmail.com>
2020-12-25 01:57:54 +0530

    finally

--- programming_fundamentals/root_prog.c
+++ programming_fundamentals/root_prog.c
@@ -3,12 +3,13 @@
 int main()
 {
 
-double value, n, root;
+double value, n, root,b;
     printf("enter the value of under root:");
     scanf("%d", &value);
     printf("enter the nth value:");
     scanf("%d", &n);
-    root = sqrt(value);
-    printf("the value of under root is:", root);
+    b=1/n;
+    root = pow(value,b);
+    printf("the value of under root is:%d", root);
     return 0;
 }
--- programming_fundamentals/swappin_prog2.c
+++ programming_fundamentals/swappin_prog2.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    char first_char,second_char;
-printf("enter the first character value:");
-scanf("%c",&first_char);
-printf("enter the second char value:");
-scanf("%d",&second_char);
-first_char=first_char+second_char;
-second_char=first_char-second_char;
-first_char=first_char-second_char;
-printf;
-
-///pending
+    char first_char, second_char;
+    printf("enter the first character value:");
+    scanf("%c", &first_char);
+    printf("enter the second char value:");
+    scanf("%d", &second_char);
+    first_char = first_char + second_char;
+    second_char = first_char - second_char;
+    first_char = first_char - second_char;
+    printf("the altered values are: %c and %c", first_char, second_char);
 
     return 0;
 }
