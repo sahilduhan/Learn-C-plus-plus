45fe6c5036c9ca3d4c7f39db08ee342ad5b99646
sahilduhan <sahilduhan16092001@gmail.com>
2021-02-11 11:22:38 +0530

    update programming

---
+++ programming_fundamentals/factorial_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+long int factorial_num(int num);
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    printf("Factorial of the num %d is  = %ld", num, factorial_num(num));
+    return 0;
+}
+
+long int factorial_num(int num)
+{
+    if (num >= 1)
+        return num * factorial_num(num - 1);
+    else
+        return 1;
+}
