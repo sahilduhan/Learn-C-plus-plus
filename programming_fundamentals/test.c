cda5fc4bf64b7c9471ddbaab8b38426c96315783
Sachin Duhan <78694755+duhan-sachin@users.noreply.github.com>
2021-03-05 14:31:24 +0530

    update

--- programming_fundamentals/test.c
+++ programming_fundamentals/test.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+
 int main()
 {
-    int n;
-    for (n = 0; n != 0; n--)
+    char string_val[1000] = "The passive voice for shut the door would be 'Let the door be shut'.", del_val[] = "the", new_array[10][30];
+    int i = 0, j = 0, k = 0, len1 = 0, len2 = 0;
+
+    // converting string to an 2D array
+    for (i = 0; string_val[i] != '\0'; i++)
     {
-        printf(" n=%d ", n--);
+        if (string_val[i] == ' ')
+        {
+            new_array[k][j] = '\0';
+            k++;
+            j = 0;
+        }
+        else
+        {
+            new_array[k][j] = string_val[i];
+            j++;
+        }
     }
+
+    new_array[k][j] = '\0';
+
+    j = 0;
+    for (i = 0; i < k + 1; i++)
+    {
+        if (strcmpi(new_array[i], del_val) == 0)
+        {
+            new_array[i][j] = 'a';
+        }
+    }
+
+    j = 0;
+
+    for (i = 0; i < k + 1; i++)
+    {
+        if (new_array[i][j] == 'a')
+            continue;
+        else
+            printf("%s ", new_array[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
