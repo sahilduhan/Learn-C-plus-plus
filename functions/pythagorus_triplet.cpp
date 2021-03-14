16dce67f6bdee7f4c61ad1eeb607f8b737bfa57f
Sachin Duhan <78694755+duhan-sachin@users.noreply.github.com>
2021-03-15 01:12:34 +0530

    update

--- functions/pythagorus_triplet.cpp
+++ functions/pythagorus_triplet.cpp
@@ -1,7 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool Check(int num_1, int num_2, int num_3)
+{
+    int x, y, z;
+    int a = max(x, max(y, z));
+    int b, c;
+    if (a == x)
+    {
+        b = y;
+        c = z;
+    }
+    else if (b == y)
+    {
+        a = x;
+        c = z;
+    }
+    else
+    {
+
+        b = x;
+        c = y;
+    }
+    if (a * a == b * b + c * c)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 int main()
 {
+    int num_1, num_2, num_3;
+    cout << " enter the 3 nums" << endl;
+    cin >> num_1 >> num_2 >> num_3;
+    if (Check(num_1, num_2, num_3))
+    {
+        cout << " pythagorus triplet " << endl;
+    }
+    else
+    {
+        cout << " not a pythagorus triplet " << endl;
+    }
 
     return 0;
 }
