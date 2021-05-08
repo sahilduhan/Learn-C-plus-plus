b11a98d3d6216192ca633f976844e82883372f8f
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-08 23:58:17 +0530

    git update

--- pepcoding_problem/reverse_array.cpp
+++ pepcoding_problem/reverse_array.cpp
@@ -1,11 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+void reverse(int *array, int size)
+{
+
+    int start = 0;
+    int end = size - 1;
+    while (start < end)
+    {
+        int i = start;
+        int j = end;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+    }
+}
 int main()
 {
     int array[] = {1, 2, 3, 4, 5};
     int size = sizeof(array) / sizeof(array[0]);
-    int start = 0;
-    int end = size - 1;
-
+    reverse(array, size);
     return 0;
 }
